8a65f4705a08edbe8dc5f37b8e151de81ae7c7d6
thangbeo2812 <namthangmu123@gmail.com>
2021-04-17 00:11:00 +0700

    00.10 17/4/2021

--- app/src/main/java/com/thanguit/imusic/activities/MainActivity.java
+++ app/src/main/java/com/thanguit/imusic/activities/MainActivity.java
@@ -2,28 +2,47 @@ package com.thanguit.imusic.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.Toast;
+import android.util.Base64;
+import android.util.Log;
 
+import com.facebook.CallbackManager;
+import com.facebook.login.widget.LoginButton;
 import com.thanguit.imusic.R;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView imageView;
+    private CallbackManager callbackManager;
+    private LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.imageView = (ImageView) findViewById(R.id.imvLove);
-        this.imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "Love", Toast.LENGTH_SHORT).show();
-            }
-        });
+
+//        Get Hash Key
+//        try {
+//            PackageInfo info = getPackageManager().getPackageInfo(
+//                    "com.thanguit.imusic",
+//                    PackageManager.GET_SIGNATURES);
+//            for (Signature signature : info.signatures) {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//            }
+//        } catch (PackageManager.NameNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (NoSuchAlgorithmException e) {
+//            e.printStackTrace();
+//        }
+
+
     }
 }
