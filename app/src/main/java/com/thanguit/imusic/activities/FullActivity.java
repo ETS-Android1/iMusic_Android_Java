904612dd6e482e5549ce78427ccad28bbfe11047
thangbeo2812 <namthangmu123@gmail.com>
2021-04-28 00:28:07 +0700

    00:27 28/04/2021

--- app/src/main/java/com/thanguit/imusic/activities/FullActivity.java
+++ app/src/main/java/com/thanguit/imusic/activities/FullActivity.java
@@ -1,6 +1,7 @@
 package com.thanguit.imusic.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +11,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.airbnb.lottie.LottieAnimationView;
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 import com.facebook.AccessToken;
 import com.facebook.login.LoginManager;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -18,7 +21,11 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserInfo;
 import com.squareup.picasso.Picasso;
 import com.thanguit.imusic.R;
-import com.thanguit.imusic.models.User;
+import com.thanguit.imusic.fragments.ChartFragment;
+import com.thanguit.imusic.fragments.HomeFragment;
+import com.thanguit.imusic.fragments.RadioFragment;
+import com.thanguit.imusic.fragments.SettingFragment;
+import com.thanguit.imusic.fragments.SingerFragment;
 
 public class FullActivity extends AppCompatActivity {
 //    private FirebaseAuth firebaseAuth;
@@ -32,6 +39,15 @@ public class FullActivity extends AppCompatActivity {
 //    private TextView Email;
 //    private Button Signout;
 
+    private MeowBottomNavigation meowBottomNavigation;
+    private Fragment fragment;
+
+    private final int ID_HOME = 1;
+    private final int ID_SINGER = 2;
+    private final int ID_CHART = 3;
+    private final int ID_RADIO = 4;
+    private final int ID_SETTING = 5;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,6 +58,13 @@ public class FullActivity extends AppCompatActivity {
     }
 
     private void Mapping() {
+        this.meowBottomNavigation = (MeowBottomNavigation) findViewById(R.id.bottomNavigation);
+
+        this.meowBottomNavigation.add(new MeowBottomNavigation.Model(this.ID_HOME, R.drawable.ic_home));
+        this.meowBottomNavigation.add(new MeowBottomNavigation.Model(this.ID_SINGER, R.drawable.ic_singer));
+        this.meowBottomNavigation.add(new MeowBottomNavigation.Model(this.ID_CHART, R.drawable.ic_chart));
+        this.meowBottomNavigation.add(new MeowBottomNavigation.Model(this.ID_RADIO, R.drawable.ic_radio));
+        this.meowBottomNavigation.add(new MeowBottomNavigation.Model(this.ID_SETTING, R.drawable.ic_setting));
 
 //        this.Avatar = findViewById(R.id.imvAvatar);
 //        this.ID = findViewById(R.id.tvID);
@@ -95,7 +118,43 @@ public class FullActivity extends AppCompatActivity {
     }
 
     private void Event() {
+        this.meowBottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+                Toast.makeText(FullActivity.this, "Fragment: " + item.getId(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
+        this.meowBottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+                fragment = null;
+                switch (item.getId()) {
+                    case 1: {
+                        fragment = new HomeFragment();
+                        break;
+                    }
+                    case 2: {
+                        fragment = new SingerFragment();
+                        break;
+                    }
+                    case 3: {
+                        fragment = new ChartFragment();
+                        break;
+                    }
+                    case 4: {
+                        fragment = new RadioFragment();
+                        break;
+                    }
+                    case 5: {
+                        fragment = new SettingFragment();
+                        break;
+                    }
+                }
+                loadFragment(fragment);
+            }
+        });
+        this.meowBottomNavigation.show(this.ID_CHART, true);
 //        Signout.setOnClickListener(new View.OnClickListener() {
 //            @Override
 //            public void onClick(View v) {
@@ -120,5 +179,12 @@ public class FullActivity extends AppCompatActivity {
 //                finish();
 //            }
 //        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frameLayout, fragment)
+                .commit();
     }
 }
