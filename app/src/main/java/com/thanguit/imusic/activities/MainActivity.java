5ff8f8abc642fd57b9cac8d6cb6741011942ec50
Chu Nam Thắng <namthangmu123@gamil.com>
2022-04-01 10:42:48 +0700

    10:42 1/4/2022

--- app/src/main/java/com/thanguit/imusic/activities/MainActivity.java
+++ app/src/main/java/com/thanguit/imusic/activities/MainActivity.java
@@ -3,11 +3,7 @@ package com.thanguit.imusic.activities;
 import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.os.Bundle;
-import android.util.Base64;
 import android.util.Log;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -52,8 +48,6 @@ import com.thanguit.imusic.animations.LoadingDialog;
 import com.thanguit.imusic.animations.ScaleAnimation;
 import com.thanguit.imusic.models.User;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -99,19 +93,16 @@ public class MainActivity extends AppCompat {
 
         DataLocalManager.init(this);
 
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                String hashKey = new String(Base64.encode(md.digest(), 0));
-                Log.d("KEY", "printHashKey() Hash Key: " + hashKey);
-            }
-        } catch (NoSuchAlgorithmException e) {
-            Log.e("Key", "printHashKey()", e);
-        } catch (Exception e) {
-            Log.e("Key", "printHashKey()", e);
-        }
+//        try {
+//            @SuppressLint("PackageManagerGetSignatures") PackageInfo info = getPackageManager().getPackageInfo("com.thanguit.imusic", PackageManager.GET_SIGNATURES);
+//            for (Signature signature : info.signatures) {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//            }
+//        } catch (PackageManager.NameNotFoundException e) {
+//        } catch (NoSuchAlgorithmException e) {
+//        }
 
         PermissionListener permissionlistener = new PermissionListener() {
             @Override
